4e87f90b511f5b06ff5650c82c17baf8fe60ec59
vinayak6387 <chauhanvinayak@918gmail.com>
2024-11-22 16:41:06 +0530

    pro add

--- reactrouterwithgithub/src/components/FetchGitData.js
+++
@@ -1,8 +0,0 @@
-const FetchGitData = async() => {
-  let response = await fetch('https://api.github.com/users/vinayak6387');
-  let data = await response.json();
-  return data;
-
-}
-
-export default FetchGitData;
